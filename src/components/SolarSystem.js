62ffaa74fc863f7c878b420051844d19c37d32bf
Érica Guimarães <eriquinha_guima@yahoo.com.br>
2023-03-31 14:39:22 -0300

    Requisitos 1 e 2

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SolaSystem extends React.Component {
+    render() {
+        return (
+            <div data-testid="solar-system">
+
+            </div>
+        );
+    }
+}
+
+export default SolaSystem;
